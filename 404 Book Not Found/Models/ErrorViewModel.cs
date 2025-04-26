14cd812cbe249027655af5fb4af90e61d146416b
Thridioux <donotdisturbyo343@gmail.com>
2025-04-26 14:06:52 +0300

    Add project files.

---
+++ 404 Book Not Found/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _404_Book_Not_Found.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
